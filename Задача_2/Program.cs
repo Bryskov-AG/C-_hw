00eaa9ffbddb666658a8e77c724e41fdc1b4b264
Bryskov-AG <santei31334@gmail.com>
2023-02-02 17:14:02 +0100

    Сделал первое ДЗ

---
+++ Задача_2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдает, какое число большее, а какое меньшее.
+
+Console.Clear();
+
+Console.WriteLine("Введите первое число a: ");
+int a = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите второе число b: ");
+int b = int.Parse(Console.ReadLine()!);
+
+int max = a;
+int min = b;
+
+if (a>max) max = a;
+if (b>max) max = b;
+if (a<max) min = a;
+if (b<max) min = b;
+
+
+Console.Write("max = ");
+Console.WriteLine(max);
+Console.Write("min = ");
+Console.WriteLine(min);
---
+++ Задача_4/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдает максимальное из этих чисел.
+
+Console.Clear();
+
+Console.WriteLine("Введите первое число a: ");
+
+int a = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите второе число b : ");
+
+int b = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите третье число с : ");
+
+int c = int.Parse(Console.ReadLine()!);
+
+int max = a;
+int min = b;
+
+if (a>max) max = a;
+if (b>max) max = b;
+if (c>max) max = c;
+
+Console.Write("max = ");
+Console.WriteLine(max);
+Console.Write("min = ");
+Console.WriteLine(min);
+
+
+
