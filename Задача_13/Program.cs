bc409421485751e9a90c619dea80299433c0cf44
Bryskov-AG <santei31334@gmail.com>
2023-02-04 22:07:35 +0100

    Вторая домашка

---
+++ Задача_13/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает,что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Clear();
+Console.WriteLine("Введите число : ");
+int a = int.Parse(Console.ReadLine()!);
+
+if (a < 0)
+{
+    a = -a;
+}
+if (a < 100)
+{
+    Console.WriteLine("Число является двузначным,нет третьей цифры.");
+} 
+else if (a < 1000) {
+    Console.WriteLine("Число является трёхзначным.");
+    int c = (a  % 10);
+    Console.WriteLine(c);
+}
+else
+{
+    int b = a;
+    while (b > 999) 
+    { 
+          b = b / 10;
+    }
+    Console.WriteLine("Число является трёхзначным.");
+    int c = (b  % 10);
+    Console.WriteLine(c);
+}
---
+++ Задача_15/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+ Console.Clear();
+
+Console.WriteLine("Введите число от 1 до 7 : ");
+int x = int.Parse(Console.ReadLine()!);
+
+// int a = 1; // Понедельник
+// int b = 2; // Вторник
+// int c = 3; // Среда
+// int d = 4; // Четверг
+// int e = 5; // Пятница
+// int f = 6; // Суббота
+// int g = 7; // Воскресенье
+
+// if (x==a) Console.WriteLine("Эх,будни.");
+// if (x==b) Console.WriteLine("Эх,будни.");
+// if (x==c) Console.WriteLine("Эх,будни.");
+// if (x==d) Console.WriteLine("Эх,будни.");
+// if (x==e) Console.WriteLine("Эх,будни.");
+// if (x==f) Console.WriteLine("Выходной!");
+// if (x==g) Console.WriteLine("Выходной!");
+
+if (x > 5 && x < 8) 
+{
+    Console.WriteLine("Выходной!") ;
+} 
+else if (x > 0 && x < 6 )
+{
+    Console.WriteLine("Эх,будни.");
+}
+else if (x < 1 ||  x > 7)
+{
+    Console.WriteLine("Не правильно набран номер!");
+}
