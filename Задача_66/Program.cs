73654eeb49579147f97c5b20ea60ad0035a2dfb1
Bryskov-AG <santei31334@gmail.com>
2023-03-04 00:05:03 +0100

    +64,66,68

---
+++ Задача_66/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+int M = 1; 
+int N = 15; 
+
+
+int sum = SumNaturalNumbers(M, N); 
+Console.WriteLine("Сумма натуральных чисел от {0} до {1}: {2}", M, N, sum);
+    
+
+int SumNaturalNumbers(int m, int n)
+    {
+        if (m == n) 
+        {
+            return m; 
+        }
+        else
+        {
+            return m + SumNaturalNumbers(m + 1, n); 
+        }
+    }
---
+++ Задача_68/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+
+int M = 3;
+int N = 2;
+
+Console.WriteLine(AckermannFun(M, N));
+
+
+int AckermannFun(int m, int n)
+{
+    if(m == 0) return n + 1;
+    else if (n == 0) 
+    return AckermannFun(m - 1, 1);
+    else 
+    return AckermannFun(m - 1, AckermannFun(m, n - 1));
+}
+
+
+
+
+
